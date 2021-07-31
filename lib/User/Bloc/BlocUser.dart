101960439d3b3793106d55559e4b8fa83eca3f51
Bryan <liga_nos@hotmail.com>
2021-07-30 20:08:34 -0500

    Final

--- lib/User/Bloc/BlocUser.dart
+++ lib/User/Bloc/BlocUser.dart
@@ -4,11 +4,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class BlocUSer implements Bloc{
   final autRepository = AutRepository();
-
+  //Stream es un flujo de datos asincronica  , instance para instanciar la class firebase y el auth para subcribir los cambios
   Stream <User?> streamFirebase= FirebaseAuth.instance.authStateChanges();
   Stream <User?>? get authStatus => streamFirebase;
 
     Future <UserCredential> singIn(){
+      //se usa pra el inicio de sesion
       return autRepository.singInFirebase();
     }
   @override
--- lib/User/Repository/AutRepository.dart
+++ lib/User/Repository/AutRepository.dart
@@ -2,6 +2,8 @@ import 'AutoFireBase.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AutRepository{
+  //_state sea privada y solo se instancie de StatefulWidget
   final _authFirebase = AutoFireBase();
+  //crea una nueva cuenta de usuario con la dirección de correo electrónico y la contraseña proporcionadas
   Future <UserCredential> singInFirebase() => _authFirebase.singIn();
 }
